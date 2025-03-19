622739776eda91b44a5310185089e3bf24549aca
Vivek Singh <vivek.singh.6967@gmail.com>
2025-03-19 20:20:56 +0530

    added logic for sorting list

--- src/dsa/itemRankMerging.rs
+++ src/dsa/itemRankMerging.rs
@@ -11,18 +11,19 @@ type pointer<T> = Option<Box<Node<T>>>;
 #[derive(Debug)]
 struct LinkedList<T: std::fmt::Debug> {
     head: pointer<T>,
-    root: pointer<T>,
+    // root: pointer<T>,
 }
 
 pub struct ItemObjectsLinkedList<T: std::fmt::Debug> {
     itemObejectList: HashMap<String, LinkedList<T>>,
+    sortedListName : String
 }
 
 impl<T: std::fmt::Debug> LinkedList<T> {
     fn create_empty_list() -> LinkedList<T> {
         LinkedList {
             head: None,
-            root: None,
+            // root: None,
         }
     }
 
@@ -33,19 +34,19 @@ impl<T: std::fmt::Debug> LinkedList<T> {
             next: previous_head,
         });
         self.head = Some(new_head);
-        // println!("current Head :{:?}",self.head.as_ref().unwrap().element);
+        // //println!("current Head :{:?}",self.head.as_ref().unwrap().element);
     }
 
     fn remove_item(&mut self) -> Option<T> {
         let previous_head = self.head.take();
-        println!("previous head: {:?} ", previous_head);
+        //println!("previous head: {:?} ", previous_head);
         match previous_head {
             Some(old_head) => {
                 self.head = old_head.next;
                 Some(old_head.element)
             }
             None => {
-                println!("NONE");
+                //println!("NONE");
                 None},
         }
     }
@@ -55,7 +56,7 @@ impl<T: std::fmt::Debug> LinkedList<T> {
         while true {
             match list_traversal {
                 Some(Node) => {
-                    print!("{:?}-->", Node.element);
+                    print!("{:?}<--", Node.element);
                     list_traversal = &list_traversal.as_ref().unwrap().next;
                 }
                 None => {
@@ -69,9 +70,10 @@ impl<T: std::fmt::Debug> LinkedList<T> {
 }
 
 impl<T: std::fmt::Debug+Clone> ItemObjectsLinkedList<T> {
-    pub fn new() -> ItemObjectsLinkedList<T> {
+    pub fn new(sortedListName: String) -> ItemObjectsLinkedList<T> {
         ItemObjectsLinkedList {
             itemObejectList: HashMap::new(),
+            sortedListName
         }
     }
 
@@ -83,8 +85,9 @@ impl<T: std::fmt::Debug+Clone> ItemObjectsLinkedList<T> {
     pub fn upsert_item(&mut self, key: &str, value: T) {
         match self.itemObejectList.get_mut(key) {
             Some(list) => {
-                // println!("{}::{:?} ",key ,value);
+                // //println!("{}::{:?} ",key ,value);
                 list.add_item(value);
+                // list.//printing();
             }
             None => {
                 self.add_linked_list(key);
@@ -96,14 +99,16 @@ impl<T: std::fmt::Debug+Clone> ItemObjectsLinkedList<T> {
     pub fn remove_item(&mut self, key: &str) {
         match self.itemObejectList.get_mut(key) {
             Some(l_list) => {
-                println!("{}:: ", key);
+                //println!("{}:: ", key);
                 l_list.remove_item();
+                // l_list.//printing();
                 if l_list.head.is_none() {
                     self.itemObejectList.remove(key);
                 }
+                // l_list.//printing();
             }
             None => {
-                println!("{} No such key exists", key);
+                //println!("{} No such key exists", key);
             }
         }
     }
@@ -123,11 +128,14 @@ impl<T: std::fmt::Debug+Clone> ItemObjectsLinkedList<T> {
     pub fn get_heads(&self) -> Vec<(T, String)> {
         let mut result = Vec::new();
         for (key, list) in &self.itemObejectList {
+            if *key == self.sortedListName {
+                continue;
+            }
             if let Some(node) = &list.head {
                 result.push((node.element.clone(), key.clone()));
             }
         }
-        println!("lengthL {:?} ", result.len());
+        // //println!("lengthL {:?} ", result.len());
         result
     }
 }
